b25e6e8ab58b688588ff16bfb2f1548ecd90a6ec
drsanti <santi.inc.kmutt@gmail.com>
2023-05-16 15:22:06 +0700

    Create final_lect0_template.svelte

---
+++ final_lect0_template.svelte
@@ -0,0 +1,26 @@
+<!-- Lec0-template -->
+<script lang="ts">
+	import { mcuLink } from "$lib/mcuLink";
+	import { onDestroy, onMount } from "svelte";
+	let leds: McuLedData[] = [];
+	let psws: McuPswData[] = [];
+	let adcs: McuAdcData[] = [];
+	onMount(() => {
+		mcuLink.start( (data: McuData) => {
+			leds = data.led;
+			psws = data.psw;
+			adcs = data.adc;
+		});
+	});
+	onDestroy(() => {
+		mcuLink.stop();
+	});
+</script>
+
+
+<div>Template</div>
+	
+
+<style>
+	
+</style>
